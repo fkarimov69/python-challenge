88319310bd6822556d191c115f0a1566238299d4
Firouz Karimov <fkarimov@gmail.com>
2019-12-23 15:13:15 -0700

    final draft 2

--- PyPoll/CodePoll.py
+++ PyPoll/CodePoll.py
@@ -21,10 +21,12 @@ with open('election_data.csv','r') as f:
 #Printing list of candidates
     result = [candidate[2] for candidate in mylist]
     #print(result[:10])
-    #result_i=result[:10]
+    result_i=result[:100]
     #print(result_i)  #result_ is a sample
 #counting votes for each candidate
-    votes={i:result.count(i) for i in result}
+    votes={i:result_i.count(i) for i in result_i}
+    #votes={candidate:mylist[2].count(candidate) for candidate in mylist}
+    #votes=votes[:10]
     #print(votes)
 
     #converting dictionary result into lists
@@ -38,15 +40,21 @@ with open('election_data.csv','r') as f:
 
     #calculate % votes
     percent=[x/total_votes*100 for x in values]
+    percent=["%.2f" % i for i in percent]
+    percent_symb=['%']*len(percent)
+    #print(percent_symb)
+    #percent=percent("%.3f%%" % ([x/total_votes*100 for x in values]))
     #print(percent)
     
     #creating list of tuples
-    sumList=list(zip(keys,values,percent))
-    #for j in sumList
-    print(*sumList,sep='\n')
+    sumList=list(zip(keys,values,percent,percent_symb))
+    #print(sumList)
+    for i in sumList:
+        print(*i,sep=' ')
     
     sort_winner=sorted(sumList,key=lambda tup:tup[1])
     winner=sort_winner[len(sort_winner)-1]
+    ##print(*sumList,sep='\n')
     print('-------------------------')
     print('Winner')
     print(winner[0])
@@ -58,11 +66,13 @@ with open('elections.txt','w') as f:
     print('----------------------------',file=f)
     print('Total Votes:  '+str(total_votes),file=f)
     print('-------------------------------',file=f)
-    print(*sumList,sep='\n')
-    print('-------------------------')
-    print('Winner')
-    print(winner[0])
-    print('---------------------------')
+    #print(*sumList,sep='\n',file=f)
+    for i in sumList:
+        print(*i,sep=' ',file=f)
+    print('-------------------------',file=f)
+    print('Winner',file=f)
+    print(winner[0],file=f)
+    print('---------------------------',file=f)
 
 
 
