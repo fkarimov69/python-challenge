9f8dc84526c54b5e3f52f6e1aad536499e052d18
Firouz Karimov <fkarimov@gmail.com>
2020-01-16 13:44:36 -0700

    reworked code

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -19,9 +19,9 @@ with open('budget_data.csv') as f:
 #calculating and printingTotal numbers of months
     print("Total Months: ",len(mydict))
 
-    keys = mydict.keys() 
-    values = mydict.values() 
-   
+    keys = list(mydict.keys()) 
+    values = list(mydict.values()) 
+
     
 #convert values to integer and sum
     values=[int(i) for i in values]
@@ -32,25 +32,31 @@ with open('budget_data.csv') as f:
     print("Total: "+str(total_pl))
 
 #Calculating Average Change
-    average_change=round(total_pl/len(mydict),2)
+    
+    dif=[values[i+1]-values[i] for i in range(len(values)-1)]
+    average_change=round(sum(dif)/len(dif),2)
     print("Average Change: "+str(average_change))
 
 #Calculating Greatest Increase in Profit
-    max_i=[(value,key) for key,value in mydict.items()]
-    max_k=(max(max_i)[1])
-    max_v=(max(values))
-    print("Greatest Increase in Profits: "+str(max_k)+"   "+"("+str(max_v)+")")
+    max_i=max(dif)
+    index_max=dif.index(max_i)
+    index_key=keys[index_max+1]
+    print("Greatest Increase in Profits: "+str(index_key)+"   "+"$"+"("+str(max_i)+")")
 
 #Calculating Greatest Decrease in Profit
-    min_i=[(value,key) for key,value in mydict.items()]
+    min_i=min(dif)
+    index_min=dif.index(min_i)
+    index_val=keys[index_min+1]
+    """min_i=[(value,key) for key,value in mydict.items()]
     min_k=(min(min_i)[1])
-    min_v=(min(values))
-    print("Greatest Decrease in Profits: "+str(min_k)+"   "+"("+str(min_v)+")")
+    min_v=(min(values))"""
+    print("Greatest Decrease in Profits: "+str(index_val)+"   "+"("+"$"+str(min_i)+")")
 #printing to .txt file
 with open('out.txt', 'w') as f:
     print('Financial Analysis',file=f)
     print('----------------------------',file=f)
     print('Total Months: ',len(mydict),file=f)
     print('Average Change: '+str(average_change),file=f)
-    print('Greatest Increase in Profits: '+str(max_k)+'   '+'('+str(max_v)+')',file=f)
-    print('Greatest Decrease in Profits: '+str(min_k)+'   '+'('+str(min_v)+')',file=f)
+    print("Greatest Increase in Profits: "+str(index_key)+"   "+"$"+"("+str(max_i)+")",file=f)
+    print("Greatest Decrease in Profits: "+str(index_val)+"   "+"("+"$"+str(min_i)+")",file=f)
+ 
